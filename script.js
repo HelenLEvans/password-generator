ab1b0a62e2ac27698e16870bb17f8aeeed7e36c0
Fifinella <h.hoesley@gmail.com>
2022-11-09 09:05:35 -0800

    js script

--- script.js
+++ script.js
@@ -1,31 +1,84 @@
-// Copyright 2022 helenhoesley
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+var length;
+var number;
+var specialCharacter;
+var upperCase;
+var lowerCase;
+var selection;
+var lettersUp
 
-// Assignment code here
+const characters = ["number", "letters", "character"];
 
+number = "0123456789";
+letters = "abcdefghijklmnopqrstuvwxyz";
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+character = "!@#$%^&*)()";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
+// Begin setting password parameters
+function generatePassword() {
+    // Select # of characters for new password
+    length = prompt("Enter the number of characters you'd like for your new password (Select a number between 8-128)");
+    if (!length) {
+        alert("Please make a valid entry");
+    } else if (length < 8 || length > 128) {
+        // Function recognizes incorrect input
+        length = prompt("New password must be from 8-128 characters");
+    } else {
+        // Prompts continue when length input is valid
+        number = confirm("Include numbers in your new password?");
+        specialCharacter = confirm("Include special characters in your new password?");
+        upperCase = confirm("Include uppercase letters?");
+        lowerCase = confirm("Include lowercase letters?");
+    };
+    // for all of the above true
+    if (number && specialCharacter && upperCase && lowerCase) {
+        selection = character.concat(number, letters, lettersUp);
+    }
+    // three options selected
+    else if (number && specialCharacter && upperCase) {
+        selection = character.concat(number, lettersUp);
+    } else if (specialCharacter && upperCase && lowerCase) {
+        selection = character.concat(letters, lettersUp);
+    } else if (upperCase && lowerCase && number) {
+        selection = lettersUp.concat(letters, number);
+    } else if (lowerCase && number && specialCharacter) {
+        selection = letters.concat(number, character);
+    }
+    //two options selected
+    else if (specialCharacter && number) {
+        selection = character.concat(number);
+    } else if (number && upperCase) {
+        selection = number.concat(lettersUp);
+    } else if (upperCase && lowerCase) {
+        selection = lettersUp.concat(letters);
+    } else if (lowerCase && number) {
+        selection = letters.concat(number);
+    } else if (lowerCase && specialCharacter) {
+        selection = letters.concat(character);
+    } else if (specialCharacter && upperCase) {
+        selection = character.concat(lettersUp);
+    }
+    //one option
+    else if (specialCharacter) {
+    else if (specialCharacter) {
+            selection = character;
+        } else if (number) {
+            selection = number;
+        } else if (lowerCase) {
+            selection = letters;
+        } else if (upperCase) {
+            selection = lettersUp;
+        };
+        return selection;
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
